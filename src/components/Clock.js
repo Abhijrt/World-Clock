4177bf0248147097d94f6aa7857e7f6035f4c822
Abhay Jirati <abhi.jrt12@gmail.com>
2020-09-25 13:37:27 +0530

    adding the time diff functionality

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -13,17 +13,126 @@ export default class Clock extends Component {
   constructor() {
     super();
     this.state = {
-      indiaTime: new Date().toTimeString().substring(0, 8),
+      indiaOffSet: 5.5,
+      londonOffSet: 1,
+      usOffSet: -4,
+      indiaTime: "", //new Date().toTimeString().substring(0, 8),
       londonTime: "",
       usTime: "",
+      indiaDiff: "",
+      londonDiff: "",
+      usSetTime: "",
     };
   }
 
+  componentDidMount() {
+    const { indiaOffSet, londonOffSet, usOffSet } = this.state;
+    var date = new Date();
+    // console.log("Date", date);
+    var localTime = date.getTime();
+    // console.log("LocalTIme", localTime);
+    var localOffSet = date.getTimezoneOffset() * 60000;
+    // console.log("LocalOFFSet", localOffSet);
+    var utc = localOffSet + localTime;
+    // console.log("UTC", utc);
+    var indiaTime = new Date(utc + 3600000 * indiaOffSet);
+    var londonTime = new Date(utc + 3600000 * londonOffSet);
+    var usTime = new Date(utc + 3600000 * usOffSet);
+    var india = indiaTime.toLocaleString().substring(12);
+    var london = londonTime.toLocaleString().substring(12);
+    var us = usTime.toLocaleString().substring(12);
+    this.setState({
+      indiaTime: india,
+      londonTime: london,
+      usTime: us,
+      indiaDiff: "+10",
+      londonDiff: "+5",
+    });
+  }
+
+  handleChange = (name, value) => {
+    // console.log(name, value);
+    if (name === "usSetTime") {
+      this.setState({ usSetTime: value });
+    } else if (name === "londonDiff") {
+      this.setState({ londonDiff: value });
+    } else if (name === "indiaDiff") {
+      this.setState({ indiaDiff: value });
+    }
+  };
+
+  handleUsTime = () => {
+    const { usSetTime } = this.state;
+    if (usSetTime.length === 0) {
+      return;
+    }
+    var arr = usSetTime.split(":");
+    var india, london;
+    if (parseInt(arr[0]) <= 14) {
+      india = parseInt(arr[0]) + 9 + ":" + arr[1] + ":" + arr[2];
+      london = parseInt(arr[0]) + 5 + ":" + arr[1] + ":" + arr[2];
+    } else {
+      if (parseInt(arr[0]) < 18) {
+        var hour = parseInt(arr[0] + 9);
+        india = (hour % 24) + ":" + arr[1] + ":" + arr[2];
+        london = parseInt(arr[0]) + 5 + ":" + arr[1] + ":" + arr[2];
+      } else {
+        var hour = parseInt(arr[0]);
+        india = ((hour + 9) % 24) + ":" + arr[1] + ":" + arr[2];
+        london = ((hour + 5) % 24) + ":" + arr[1] + ":" + arr[2];
+      }
+    }
+    this.setState({
+      indiaTime: india,
+      londonTime: london,
+      usTime: usSetTime,
+      indiaDiff: "+9",
+      londonDiff: "+5",
+    });
+
+    // console.log(india, london);
+  };
+
+  handleLondonDiff = () => {
+    const { londonDiff, usTime } = this.state;
+    var us = usTime.split(":");
+    var london = parseInt(us[0]) + parseInt(londonDiff);
+    // console.log(london);
+    var londonNewTime;
+    if (london < 0) {
+      londonNewTime = 24 + london + ":" + us[1] + ":" + us[2];
+    } else {
+      londonNewTime = london + ":" + us[1] + ":" + us[2];
+    }
+    this.setState({
+      londonTime: londonNewTime,
+    });
+  };
+
+  handleIndiaDiff = () => {
+    const { indiaDiff, usTime } = this.state;
+    var us = usTime.split(":");
+    var india = parseInt(us[0]) + parseInt(indiaDiff);
+    // console.log(london);
+    var indiaNewTime;
+    if (india < 0) {
+      indiaNewTime = 24 + india + ":" + us[1] + ":" + us[2];
+    } else {
+      indiaNewTime = india + ":" + us[1] + ":" + us[2];
+    }
+    this.setState({
+      indiaTime: indiaNewTime,
+    });
+  };
+
   render() {
     const { isLoggedIn } = this.props;
+    const { indiaTime, londonTime, usTime, indiaDiff, londonDiff } = this.state;
     if (!isLoggedIn) {
       return <Redirect to="/" />;
     }
+    // console.log(usSetTime);
+    // console.log("INdia ******", indiaTime, londonTime, usTime);
     return (
       <div className="my-5">
         <InputGroup className="mb-3">
@@ -33,10 +142,18 @@ export default class Clock extends Component {
             </InputGroup.Prepend>
           </Row>
           <Row sm={12} className="ml-5 mb-3">
-            <FormControl placeholder="12:45:55" className="ml-5" />
+            <FormControl
+              onChange={(e) => this.handleChange("usSetTime", e.target.value)}
+              placeholder="12:45:55"
+              className="ml-5"
+            />
           </Row>
           <Row sm={12} className="ml-5 mb-3">
-            <Button className="ml-5" variant="primary" type="submit">
+            <Button
+              className="ml-5"
+              variant="primary"
+              onClick={this.handleUsTime}
+            >
               Submit
             </Button>
           </Row>
@@ -44,13 +161,13 @@ export default class Clock extends Component {
         <Container className="my-5">
           <Row>
             <Col xs={12} md={4}>
-              United State : <span>16:34:56</span>
+              United State : <span style={{ fontSize: 40 }}>{usTime}</span>
             </Col>
-            <Col xs={12} md={4}>
-              London : <span>16:34:56</span>
+            <Col xs={12} md={4} className="my-4">
+              London : <span>{londonTime}</span>
             </Col>
-            <Col xs={12} md={4}>
-              India : <span>16:34:56</span>
+            <Col xs={12} md={4} className="my-4">
+              India : <span>{indiaTime}</span>
             </Col>
           </Row>
         </Container>
@@ -61,19 +178,31 @@ export default class Clock extends Component {
           <Row>
             Time Difference For London
             <Col xs={12} md={2}>
-              <FormControl className="mb-3" placeholder="-55" />
+              <FormControl
+                className="mb-3"
+                placeholder="-55"
+                value={londonDiff}
+                onChange={(e) =>
+                  this.handleChange("londonDiff", e.target.value)
+                }
+              />
             </Col>
             <Col>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" onClick={this.handleLondonDiff}>
                 Submit
               </Button>
             </Col>
             Time Difference For India
             <Col xs={12} md={2}>
-              <FormControl placeholder="-55" className="mb-3" />
+              <FormControl
+                placeholder="-55"
+                className="mb-3"
+                value={indiaDiff}
+                onChange={(e) => this.handleChange("indiaDiff", e.target.value)}
+              />
             </Col>
             <Col>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" onClick={this.handleIndiaDiff}>
                 Submit
               </Button>
             </Col>
